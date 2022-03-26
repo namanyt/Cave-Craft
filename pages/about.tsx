b5ced0937fe4e533244fe148ec0cb84bc7fbdda9
Cider <cider@ciderboi.xyz>
2022-03-26 20:25:58 +0530

    updated about me

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,18 +1,112 @@
 import { NextPage } from "next";
 import styles from '../styles/About.module.scss';
 
+import { AnimatePresence, motion, useViewportScroll } from 'framer-motion';
+import { UIEvent, useEffect, useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import Link from "next/link";
+
 const About: NextPage = () => {
+  let [visible, setVisibility] = useState(true);
+
+  const handleClick = () => {
+    setVisibility(!visible);
+  }
+
   return (
     <>
       <div className={styles.container}>
         <img src="https://i.imgur.com/XL2PAd3.png" alt="" className={styles.bgImage} />
         {/* <Image src="https://i.imgur.com/XL2PAd3.png" width='1920' height='1080' className={styles.bgImage} /> */}
 
-        <section className={styles.Header}>
-          <h1 className={styles.Title}>About Us</h1>
-        </section>
+        <AnimatePresence>
+          {
+            true && <motion.div
+              initial={{
+                opacity: 0
+              }}
 
-        {/* TODO: put scroll effect on about me */}
+              animate={{
+                opacity: 1,
+                transition: {
+                  duration: 1
+                }
+              }}
+            >
+              <Link href='/'>
+                <p className={styles.Back}>To Home</p>  
+              </Link>
+            </motion.div>
+          }
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {
+            visible && <motion.div
+              variants={{
+                hidden: {
+                  x: '100%',
+                  opacity: 0
+                },
+                visible: {
+                  x: '0%',
+                  opacity: 1,
+                  transition: {
+                    duration: 1
+                  }
+                },
+                hidden_exit: {
+                  x: "-100%",
+                  opacity: 0,
+                  transition: {
+                    duration: 1
+                  }
+                }
+              }}
+
+              initial='hidden'
+              animate='visible'
+              exit='hidden_exit'
+
+              className={styles.Header}
+              id="Header"
+            >
+              <h1 className={styles.Title} onClick={() => handleClick()}>About Us</h1>
+            </motion.div>
+          }
+        </AnimatePresence>
+        <AnimatePresence>
+          {
+            !visible && <motion.div
+              variants={{
+                hidden: {
+                  opacity: 0
+                },
+                visible: {
+                  opacity: 1,
+                  transition: {
+                    duration: 1
+                  }
+                },
+                hidden_exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 1
+                  }
+                }
+              }}
+
+              initial='hidden'
+              animate='visible'
+              exit='hidden_exit'
+            >
+              <p onClick={() => handleClick()} className={styles.Content}>
+                Toivi Complete this About us 🔫 (or dm it)
+              </p>
+            </motion.div>
+          }
+        </AnimatePresence>
+
 
       </div>
       <h1 className={styles.MobileText}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,34 +27,6 @@ const App: NextPage = () => {
   return (
     <>
       <div className={styles.container}>
-        <Head>
-          <title>Cave Craft</title>
-          <meta name="description" content="This is the Official Website for Cave Craft Server" />
-          <meta name='theme-color' content='#465194' />
-          <meta name='author' content='Cider' />
-          <meta name='keywords' content='Cave Craft, Minecraft, Survival, Spigot' />
-
-          
-          {/* Social Media Tags */}
-          <meta property="og:title" content="Cave Craft Server" />
-          <meta property="og:type" content="article" />
-          <meta property="og:url" content="https://cavecraft.in" />
-          <meta property="og:image" content="/favicon-32.png" />
-          <meta property="og:description" content="This is the Cave Craft Minecraft Server." />
-          <meta property="og:site_name" content="Cave Craft" />
-          
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name='twitter:site' content='@ciderboi123' />
-          <meta name='twitter:title' content='Cave Craft' />
-          <meta name='twitter:creator' content='@ciderboi123' />
-          <meta name='twitter:description' content='This is the Official Website for Cave Craft Server' />
-          <meta name='twitter:image:src' content='/favicon-32.png' />
-          <meta name="twitter:image:alt" content="no image text provided" />
-
-          <link rel="icon" href="/favicon-16.png" />
-        </Head>
-
-
         <nav className={styles.Nav}>
           <div className={styles.Logo}>
             <span><img src="/favicon-32.png" alt="" /> Cave Craft </span>
