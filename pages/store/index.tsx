15df3970c3a7a948bd4bd3e09b9b29c94b0b89e1
Cider <cider@ciderboi.xyz>
2022-03-31 16:20:48 +0530

    made store (placeholder) look nais

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,8 +1,24 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
+import { CSSProperties } from 'react'
+import styles from '../../styles/Store.module.scss'
 
 const Store: NextPage = () => {
-	return <Link href='/'> This is the Store Page </Link>
+	return (
+		<>
+			<img
+				src='https://i.imgur.com/XL2PAd3.png'
+				alt=''
+				className={styles.bgImage}
+			/>
+			<div className={styles.Back}>
+				<Link href='/'>
+					<h1>Store Page, Work in Progress</h1>
+				</Link>
+				<p> (click to go back home)</p>
+			</div>
+		</>
+	)
 }
 
 export default Store
